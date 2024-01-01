cacac42616b063d77ffa7750e82bd8f0529da12b
Gabriel Berrebi <gabriel.berrebi@vo2-group.com>
2024-01-01 11:50:04 +0200

    Initial Commit

---
+++ clienteling/clientelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientelingApp.swift
+//  clienteling
+//
+//  Created by Gabriel Berrebi on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct clientelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
